e71e6621dced9fcdf3ee7f26a6acf77cf2816c38
rossi-rahmayani <rossi.rahmayani@mitrais.com>
2023-03-24 13:15:09 +0700

    move method screen to each class, + init validation exception class

---
+++ src/main/java/com/rossi/javasimulatoratm/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package com.rossi.javasimulatoratm.exception;
+
+public class ValidationException extends Throwable{
+
+    public ValidationException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/rossi/javasimulatoratm/service/WithdrawService.java
+++ src/main/java/com/rossi/javasimulatoratm/service/WithdrawService.java
@@ -1,5 +1,6 @@
 package com.rossi.javasimulatoratm.service;
 
+import com.rossi.javasimulatoratm.common.WithdrawalAmount;
 import com.rossi.javasimulatoratm.model.Account;
 
 import java.math.BigInteger;
@@ -8,9 +9,48 @@ import java.time.format.DateTimeFormatter;
 
 import static com.rossi.javasimulatoratm.common.MessageConstant.INSUFFICIENT_BALANCE;
 
-public class WithdrawService {
+public class WithdrawService extends AtmService{
 
-    public Boolean withdraw(Account account, BigInteger amount){
+    protected void withdrawScreen(Account account) {
+        System.out.println(
+                "1. $10\n" +
+                "2. $50\n" +
+                "3. $100\n" +
+                "4. Other\n" +
+                "5. Back");
+        System.out.print("Please choose option[5]: ");
+        String option = input.nextLine();
+        BigInteger wa;
+
+        switch (option) {
+            case "1", "2", "3":
+                wa = WithdrawalAmount.findByCode(option).map(WithdrawalAmount::getAmount).orElse(BigInteger.ZERO);
+                checkWithdrawal(account, wa);
+                break;
+            case "4":
+                System.out.println("Other Withdraw");
+                System.out.print("Enter amount to withdraw: ");
+                String amount = input.nextLine();
+                if (validationService.validateWithdrawalAmount(amount)) {
+                    wa = new BigInteger(amount);
+                    checkWithdrawal(account, wa);
+                } else {
+                    withdrawScreen(account);
+                }
+                break;
+            default:
+                transactionScreen(account);
+        }
+    }
+
+    private void checkWithdrawal(Account account, BigInteger amount){
+        if (withdraw(account, amount)) {
+            summaryOption(account);
+        } else {
+            withdrawScreen(account);
+        }
+    }
+    protected Boolean withdraw(Account account, BigInteger amount){
         if (amount.compareTo(account.getBalance()) > 0){
             System.out.println(INSUFFICIENT_BALANCE + amount.toString());
             return Boolean.FALSE;
